ab7a9440e7cf4e328672f81c19c5296ad8e017e7
tieens1209 <nnqqtt2004@gmail.com>
2024-12-23 14:07:51 +0700

    refactor: edit css ticket

--- views/ticket.php
+++ views/ticket.php
@@ -1,4 +1,25 @@
 
-<iframe src="https://dashboard.cmsmart.net/apps/tickets/all-tickets" width="100%" height="100%" title="Support Ticket"></iframe>
+<!-- <iframe src="https://dashboard.cmsmart.net/apps/tickets/all-tickets" width="100%" height="100%" title="Support Ticket"></iframe> -->
+    <style>
+.container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  padding-top: 56.25%; /* 16:9 Aspect Ratio */
+}
 
-
+.responsive-iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
+<div class="container"> 
+  <iframe class="responsive-iframe" src="https://dashboard.cmsmart.net/apps/tickets/all-tickets"></iframe>
+</div>
